6ff2a738ab5005c6d4363a86b2eba4ff3c8f9390
lucasgpmoreira <94094183+lucasgpmoreira@users.noreply.github.com>
2024-08-29 21:00:05 -0300

    arrumado busca por CPF

--- src/main/java/com/ofenfit/ofenfit/repository/ClienteRepository.java
+++ src/main/java/com/ofenfit/ofenfit/repository/ClienteRepository.java
@@ -4,9 +4,15 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ofenfit.ofenfit.model.ClienteModel;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 //import org.springframework.stereotype.Repository;
 
 //@Repository
 public interface ClienteRepository extends JpaRepository<ClienteModel, Long> {
-      List<ClienteModel> findByNomeContainingIgnoreCaseOrCpf(String nome, String cpf);
+
+      @Query("SELECT c FROM ClienteModel c WHERE " +
+              "(:nome IS NULL OR c.nome LIKE %:nome%) AND " +
+              "(:cpf IS NULL OR c.cpf = :cpf)")
+      List<ClienteModel> buscarPorNomeOuCpf(@Param("nome") String nome, @Param("cpf") String cpf);
 }
